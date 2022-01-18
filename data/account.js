e745c03d73b5e30d49c616f493cc902e816cf90f
Jari <71637697+bennyk0406@users.noreply.github.com>
2022-01-18 23:50:44 +0900

    v1.0.0

--- data/account.js
+++ data/account.js
@@ -1,7 +1,7 @@
 const account = {
-    email: "nmhisbests@gmail.com",
-    password: "moon5479",
-    clientName: "Client3.0",
-    uuid: "YbqQG8Cgf4lucD7a2V4YuRvT6yiDJU0btPiLEmqVSu+hELrSeW/Pu2veID3PX/XwP4/PxI8jQAkuKMY0Dom+8A=="
+    email: "email",
+    password: "password",
+    clientName: "clientName",
+    uuid: "uuid"
 };
 exports.account = account;
